f7f110752d51b6d784c4825647c7955f654b28fc
himanshu8655 <47948526+himanshu8655@users.noreply.github.com>
2024-05-10 12:50:47 -0400

    added validation"

---
+++ src/app/services/book-service.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BookModel } from '../models/book-model';
+import { environment } from '../../../environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookServiceService {
+
+  constructor(private http:HttpClient) { }
+
+  addBook(book:BookModel){
+    const formData = new FormData();
+    for (const key in book) {
+      if (book.hasOwnProperty(key)) {
+        formData.append(key,book[key]);
+      }
+    }
+    return this.http.post(environment.base_url+'book',formData).subscribe();
+  }
+  getBooks(){
+    return this.http.get(environment.base_url+'books').subscribe()
+  }
+  editBook(book:BookModel){
+    const formData = new FormData();
+    for (const key in book) {
+      if (book.hasOwnProperty(key)) {
+        formData.append(key,book[key]);
+      }
+    }
+    return this.http.put(environment.base_url+'book',book).subscribe();
+  }
+
+}
