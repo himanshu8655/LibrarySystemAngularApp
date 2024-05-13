bc783ce7c560753a1174fde731682e41fd1c2f45
himanshu8655 <47948526+himanshu8655@users.noreply.github.com>
2024-05-12 20:41:08 -0400

    added auth interceptor and functionality

---
+++ src/app/interceptor/auth-interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse,HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    
+    constructor(private cookieService: CookieService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const modifiedRequest = request.clone({
+      });
+  
+      return next.handle(modifiedRequest).pipe(
+        tap((event: HttpEvent<any>) => {
+          if (event instanceof HttpResponse) {
+
+            let uid = event.body.get('uid')
+            if(uid){
+                this.cookieService.set('uid',uid)
+            }
+          }
+        }, (error: any) => {
+          if (error instanceof HttpErrorResponse) {
+            console.error('Error intercepted:', error);
+          }
+        })
+      );
+    }
+  }
