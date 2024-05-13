d83f670de013a81a4c042652bb0428035eb1118d
himanshu8655 <47948526+himanshu8655@users.noreply.github.com>
2024-05-13 00:40:00 -0400

    added loop for month and year

--- src/app/checkout-pg/checkout-pg.component.ts
+++ src/app/checkout-pg/checkout-pg.component.ts
@@ -3,16 +3,18 @@ import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { environment } from '../../../environment';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-checkout-pg',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule,CommonModule],
   templateUrl: './checkout-pg.component.html',
   styleUrl: './checkout-pg.component.css'
 })
 export class CheckoutPgComponent implements OnInit{
   bookId:string=''
+  months: number[] = Array.from({length: 12}, (_, i) => i + 1); // Creates an array [1, 2, 3, ..., 12]
 constructor(private http:HttpClient,private route: ActivatedRoute,private router: Router){}
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
@@ -28,6 +30,7 @@ constructor(private http:HttpClient,private route: ActivatedRoute,private router
 purchase(event:Event){
   event.preventDefault();
 this.http.get(environment.base_url+'/book/downlaod/'+this.bookId).subscribe(data=>{
+  alert('Book downloaded successfully')
 },err=>{
   alert("error processing request")
 }
